d839a84db5fe9619f9d29812fcb76746e7fb43a7
Hardik Saini <43683678+Guardianofgotham@users.noreply.github.com>
2020-05-08 17:31:45 +0530

    Delete getResponse.py

--- getResponse.py
+++
@@ -1,5 +0,0 @@
-import urllib3
-
-req = urllib3.Request('ftp://ftp.ncbi.nlm.nih.gov/geo/platforms/GPL24nnn/GPL24676/soft/GPL24676_family.soft.gz')
-response = urllib3.urlopen(req)
-the_page = response.read()
